20ab13b013f51c9f50e07a7dbc45d66a692886c8
Teo <teosany@icloud.com>
2024-04-03 16:01:45 +0200

    add ServiceController

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\UserService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,6 +16,21 @@ class ServiceController extends AbstractController
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ServiceController.php',
+        ]);
+    }
+    #[Route('/service/create', name: 'create_action')]
+    public function createAction(EntityManagerInterface $em): JsonResponse
+    {
+        $user = new UserService();
+        $user->setNom("Bogdan");
+        $user->setAge("26");
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'message' => 'New user',
+            'value' => $user->getNom()
         ]);
     }
 }
